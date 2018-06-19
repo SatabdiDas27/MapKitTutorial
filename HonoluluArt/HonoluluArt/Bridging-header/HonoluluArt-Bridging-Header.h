d8d01fa96c8c77c7476166d408147dc0e7644f46
Satabdi Das <satabdidas@BHUKRK458L.local>
2018-06-19 15:32:23 -0700

    pods files/frameworks

---
+++ HonoluluArt/HonoluluArt/Bridging-header/HonoluluArt-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  HonoluluArt-Bridging-Header.h
+//  HonoluluArt
+//
+//  Created by Satabdi Das on 19/06/18.
+//  Copyright © 2018 Ray Wenderlich. All rights reserved.
+//
+
+#ifndef HonoluluArt_Bridging_Header_h
+#define HonoluluArt_Bridging_Header_h
+
+#import <GoogleAnalytics/GAIDictionaryBuilder.h>
+#import <GoogleAnalytics/GAIFields.h>
+#import <GoogleAnalytics/GAI.h>
+#import <GoogleAnalytics/GAITracker.h>
+#import <GoogleAnalytics/GAILogger.h>
+#endif /* HonoluluArt_Bridging_Header_h */
